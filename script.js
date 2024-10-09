9617443833162aca20602fc67621031d6aeb191f
hannoveran <159809577+hannoveran@users.noreply.github.com>
2024-10-09 22:25:50 +0300

    ЛР3

--- script.js
+++ script.js
@@ -41,7 +41,22 @@ function sidebarButtons(labNumber){
                 <li><button id="btn9">ВИСНОВКИ до ЛР№2</button></li>
             `;
             break;
-        
+        case 3:
+            buttonsHTML = `
+                <li><button id="btn1">Тема, мета ЛР№2. Місце розташування сайту, звіту</button></li>
+                <li><button id="btn2">ЗАВДАННЯ №1. Зовнішній вигляд макету, код макету</button></li>
+                <li><button id="btn3">ЗАВДАННЯ №2</button></li>
+                <li><button id="btn4">Фіксована таблична верстка</button></li>
+                <li><button id="btn5">Гумова таблична верстка</button></li>
+                <li><button id="btn6">Фіксована блокова верстка</button></li>
+                <li><button id="btn7">Гумова блокова верстка</button></li>
+                <li><button id="btn8">ЗАВДАННЯ №3 FLEXBOX</button></li>
+                <li><button id="btn9">Скріншот сторінки (Flexbox)</button></li>
+                <li><button id="btn10">HTML-код (Flexbox)</button></li>
+                <li><button id="btn11">CSS-код (Flexbox)</button></li>
+                <li><button id="btn12">ВИСНОВКИ</button></li>
+            `;
+            break;
         default:
             buttonsHTML = '<li><p>Немає інформації для цієї лабораторної роботи</p></li>';
     }
@@ -232,7 +247,68 @@ function loadLab2Content(buttonId) {
 
     return content;
 }
+function loadLab3Content(buttonId) {
+    let content = '';
 
+    switch (buttonId) {
+        case 'btn1':
+            content = `<p>Тема:ВЕРСТКА HTML-ДОКУМЕНТУ. БЛОКОВА ВЕРСТКА.  ВЕРСТКА ЗАСОБАМИ CSS та FLEXBOX.<br> Мета: придбати практичні навички роботи  верстки сторінок засобами CSS, верстки на основі плаваючих елементів, з’ясувати переваги та недоліки типів макетів веб-сторінок придбати практичні навички роботи  верстки сторінок засобами CSS та FLEXBOX <br> Місце розташування сайту: https://github.com/hannoveran/Laba2.git <br> Місце розташування звіту: https://github.com/hannoveran/ReportDoc.git</p>`;
+            break;
+        case 'btn2':
+            content = `<p><img src="images/L3/mainPage.png"> <br> <img src="images/L3/кодГоловна1.png" width=700px> <br> <img src="images/L3/кодГоловна2.png" width=700px> <br> <img src="images/L3/кодГоловна3.png"> <br> <img src="images/L3/кодГоловна4.png"> <br> <img src="images/L3/кодГоловна5.png"></p>`;
+            break;
+        case 'btn3':
+            content = `<p>ЗАВДАННЯ №2</p>`;
+            break;
+        case 'btn4':
+            content = `<p>Фіксована таблична верстка <br> <img src="images/L3/фіксТабл1.png" width=700px> <br> <img src="images/L3/фіксТабл2.png" width=700px></p>`;
+            break;
+        case 'btn5':
+            content = `<p>Гумова таблична верстка <br> <br> <img src="images/L3/гумТабл1.png" width=700px> <br> <img src="images/L3/гумТабл2.png" width=700px></p>`;
+            break;
+        case 'btn6':
+            content = `<p>Фіксована блокова верстка <br> <br> <img src="images/L3/фіксБлок1.png" width=700px> <br> <img src="images/L3/фіксБлок2.png" width=700px></p>`;
+            break;
+        case 'btn7':
+            content = `<p>Гумова блокова верстка <br><br> <img src="images/L3/гумБлок1.png" width=700px> <br> <img src="images/L3/гумБлок2.png" width=700px> </p>`;
+            break;
+        case 'btn8':
+            content = `<p>ЗАВДАННЯ №3</p>`;
+            break;
+        case 'btn9':
+            content = `<p> <img src="images/L3/зав3сайт.png" width=700px></p>`;
+            break;
+        case 'btn10':
+            content = `<p><br> <img src="images/L3/шкод1.png" width=700px><br> <img src="images/L3/шкод2.png" width=700px><br> <img src="images/L3/шкод3.png" width=700px><br> <img src="images/L3/шкод4.png" width=700px><br> <img src="images/L3/шкод5.png" width=700px><br> <img src="images/L3/шкод6.png" width=700px></p>`;
+            break;
+        case 'btn11':
+            content = `<p><br> <img src="images/L3/скод1.png" width=700px> <br> <img src="images/L3/скод2.png" width=700px><br> <img src="images/L3/скод3.png" width=700px><br> <img src="images/L3/скод4.png" width=700px><br> <img src="images/L3/скод5.png" width=700px></p>`;
+            break;
+        case 'btn12':
+            content = `<p>ВИСНОВКИ до ЛР№3: <br> У ході виконання лабораторної роботи було розглянуто два підходи до верстки веб-додатків: верстка за допомогою таблиць та верстка блоками (зокрема плаваючими блоками і Flexbox). Обидва підходи мають свої переваги і недоліки. <br>
+            Переваги верстки за допомогою таблиць:<br>
+            - Таблиці забезпечують надійне відображення інформації у вигляді сітки, що особливо підходить для даних, які мають строгий табличний формат (наприклад, фінансові звіти або порівняльні таблиці).<br>
+            - Легке управління структурою та вирівнюванням вмісту у стовпцях і рядках.<br>
+            Недоліки верстки за допомогою таблиць:<br>
+            -Таблична верстка не є семантично правильною для побудови макетів сторінок. Вона може ускладнювати підтримку коду та робити його менш доступним для користувачів із обмеженими можливостями або для пошукових систем.<br>
+            - Верстка таблицями має низьку гнучкість. Адаптивність до різних розмірів екрану обмежена, що створює проблеми при створенні адаптивного дизайну.<br>
+            - У порівнянні з Flexbox та CSS Grid, таблиці значно важче налаштовувати під сучасні вимоги до дизайну.<br>
+            Переваги блокової верстки (включаючи плаваючі блоки та Flexbox):<br>
+            - Блокова верстка є семантично правильною та зрозумілою для браузерів і пошукових систем. <br>
+            - Flexbox дозволяє легко створювати адаптивні макети, що автоматично підлаштовуються під різні розміри екранів, що є важливим для сучасного веб-дизайну. <br>
+            - Плаваючі блоки (float) дозволяють організувати елементи на сторінці в різних комбінаціях, забезпечуючи гнучкість при розміщенні вмісту. <br>
+            - Flexbox спрощує роботу з вирівнюванням елементів, дозволяючи легко налаштовувати вертикальне та горизонтальне вирівнювання в контейнерах. <br>
+            Недоліки блокової верстки:<br>
+            - Використання плаваючих блоків (float) може призвести до труднощів у вирівнюванні елементів, особливо при створенні складних макетів. Це часто вимагало додаткових стилів або хаків для очищення потоків.<br>
+            - Незважаючи на те, що Flexbox є більш гнучким, у великих проектах може бути складно підтримувати ідеальне вирівнювання між різними секціями без добре продуманої структури CSS.<br>
+            - Загалом, блокова верстка, особливо з використанням Flexbox, є сучаснішим і ефективнішим підходом до створення веб-додатків, надаючи більше можливостей для адаптивного дизайну, зручності використання і підтримки в порівнянні з таблицями. </p>`;
+            break;
+        default:
+            content = '<p>Виберіть тему для відображення інформації.</p>';
+    }
+
+    return content;
+}
 // document.querySelectorAll('aside button').forEach(button => {
 //     button.addEventListener('click', (event) => {
 //         console.log(`Кнопка з ідентифікатором ${event.target.id} була натиснута.`);
